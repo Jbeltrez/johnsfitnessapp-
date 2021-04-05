e1056579eff07008a7be3dec5b27e6d179c390f6
Jbeltrez <beltrezjohn@gmail.com>
2021-04-05 13:54:34 -0400

    Built out sessions controller, session routes, and corresponding actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :show, :edit]
+  resources :users, only: [:show, :edit, :update, :create]
 
   resources :workouts
   #only an index for a users workouts 
   #nested resource you want a workout 
   resources :exercises
+
+  get "/signup", to: "users#new"
+  get 'login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+
+  get '/logout', to: "sessions#destroy"
   # default route 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
